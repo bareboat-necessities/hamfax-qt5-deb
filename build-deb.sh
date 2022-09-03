1305539dbb164a5b4a267195ff1b33e16a04d650
mgrouch <mgrushinskiy@gmail.com>
2022-09-03 13:07:35 -0400

    initial

--- build-deb.sh
+++ build-deb.sh
@@ -3,6 +3,7 @@
 sudo apt-get -y install qtbase5-dev qtbase5-dev-tools qttools5-dev-tools \
  dpkg-dev debhelper devscripts equivs pkg-config
 
+rm -rf hamfax-qt5
 git clone https://github.com/sergioisidoro/hamfax-qt5
 cp -r debian hamfax-qt5
 
